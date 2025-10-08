ee653e9d61ec6163d852a27a0db8dad21b8a394a
Fgb4z <fernandogb4@al.insper.edu.br>
2025-10-08 10:29:07 -0300

    mudanças

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="eo8WNkaTwrcGujuCMSkRZG3yMYuQvXTy"
+      domain="dev-tv4os4iuxn3m6jo0.us.auth0.com"
+      clientId="6cH1YwI7e5f2EPYDVWAhnPrqE5pwJzSs"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-tv4os4iuxn3m6jo0.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
